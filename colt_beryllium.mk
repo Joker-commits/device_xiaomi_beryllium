3fe1d1f83b9be7d0b428f7d8ea1e7dd31ffc9734
joker-commits <mkr896@gmail.com>
2020-07-18 21:41:22 +0000

    Bringup Colt

--- colt_beryllium.mk
+++ colt_beryllium.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Colt stuff.
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := colt_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
@@ -23,3 +23,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+
+# ColtOS stuffs
+COLT_BUILD_TYPE := Official
+TARGET_BOOT_ANIMATION_RES := 1080×1920
+COLT_DEVICE_MAINTAINER := Mani♥Madhuri
